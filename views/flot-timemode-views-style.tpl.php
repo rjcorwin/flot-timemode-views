1c53648ff6a4762ef2b59e15419710a7fdc2b4c8
R.J. Steinert <rj@rjsteinert.com>
2010-12-14 18:52:44 -0500

    Flot Timemode Views style plugin and Timemode Data point wrapped in a module.

---
+++ views/flot-timemode-views-style.tpl.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @file flot-views-summary-style.tpl.php
+ * Template to display a flot summary view.
+ *
+ * - $element : An array representation of the flot DOM element.
+ * - $data: A flotData object.
+ * - $options: A flotStyle object.
+ */
+?>
+
+<div class="views-flot">
+  <?php print theme('flot_graph', $element, $data, $options); ?>
+</div>
